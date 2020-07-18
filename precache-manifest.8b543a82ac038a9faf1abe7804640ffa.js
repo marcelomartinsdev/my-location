1f12e4e52ef499b47d4cc64fbe676e0ca83a1d19
Marcelo Martins Dev <marcelomartinsdev@gmail.com>
2020-07-18 13:14:06 -0300

    Updates

---
+++ precache-manifest.8b543a82ac038a9faf1abe7804640ffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1f5337656338dbc16e940aad278210",
+    "url": "/my-location/index.html"
+  },
+  {
+    "revision": "b7fe3834d1d0c8bf6399",
+    "url": "/my-location/static/css/main.0e86c38e.chunk.css"
+  },
+  {
+    "revision": "5ad1c3a0ff8824726451",
+    "url": "/my-location/static/js/2.a5f386e0.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/my-location/static/js/2.a5f386e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7fe3834d1d0c8bf6399",
+    "url": "/my-location/static/js/main.2d17af25.chunk.js"
+  },
+  {
+    "revision": "ccc1207b7e100026d123",
+    "url": "/my-location/static/js/runtime-main.71ff4d1e.js"
+  }
+]);

8ccdd91275007198aad321550d53fc7f839853ad
Marcelo Martins Dev <marcelomartinsdev@gmail.com>
2020-07-18 13:36:31 -0300

    react-router bug fix

--- src/Routes.js
+++ src/Routes.js
@@ -6,10 +6,8 @@ import MapPage from "./Map/MapPage";
 const Routes = () => (
   <Router>
     <Switch>
-      <Route exact path="https://marcelomartinsdev.github.io/my-location/" component={Home} />
-    </Switch>
-    <Switch>
-      <Route exact path="https://marcelomartinsdev.github.io/my-location/map" component={MapPage} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/map" component={MapPage} />
     </Switch>
   </Router>
 );
